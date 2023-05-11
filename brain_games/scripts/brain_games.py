cb23b2b5fc34e670f0e6ab32719381dce0e0ea55
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-05-11 23:10:57 +0300

    modify brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import brain_games
+from brain_games.cli import welcome_user
 
 def start_game(module):
     print("Welcome to the Brain Games!")
 
 def main():
     start_game(brain_games)
-
+    welcome_user()
 
 if __name__ == '__main__':
     main()
