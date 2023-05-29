6be8d5e17824526855d2d83656a1f50e41b310cf
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-05-29 22:05:16 +0300

    modify brain_even_logic.py and brain_even.py

--- brain_games/brain_even_logic.py
+++ brain_games/brain_even_logic.py
@@ -1,6 +1,7 @@
+from unittest import result
 import prompt
 from random import randint
-from brain_games.cli import welcome_user
+from brain_games.check_answer import check_answer
 
 
 def is_even(number):
@@ -10,33 +11,31 @@ def is_even(number):
         return False
     
 
-def check_answer():
-    if True:
-        return 'yes'
-    else:
-        return 'no'
-        
-
-def player_answer():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    number_of_attempts = 3
+def player_answers():
     
+    
+    number_of_attempts = 3
+    player_name = prompt.string('May I have your name? ')
+    print(f'Hello, {player_name}')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    
+
     for _ in range(number_of_attempts):
         
-        question_number = randint(1,450)
-        print(f'Question: {question_number}')
-        answer = prompt.string('Your answer: ')
         
-        if is_even(question_number) == True and answer == 'yes':
-            return('Correct!')
-        if is_even(question_number) == False and answer == 'no':
-            return('Correct!')
-        if is_even(question_number) == True and answer == 'no':
-            return(f''''no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, {name} !''')
+        question_number = randint(1,999)
+        print(f'Question: {question_number}')
+        
+        player_answer = prompt.string('Your answer: ')
+        even_number = is_even(question_number)
+        correct_answer = check_answer(even_number)
+
+        if player_answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f'''{player_answer} is wrong answer ;(. Correct answer was {correct_answer}. 
+Let's try again, {player_name}! ''')
             break
-        if is_even(question_number) == False and answer == 'yes':
-            return(f''''yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {name} !''')
-            break
-        print(f'''Congratulations, {name}''')
+    print(f'''Congratulations, {player_name}!''')
+    
+
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,20 +2,13 @@
 
 
 
-from brain_games.brain_even_logic import player_answer
-#from brain_games.brain_even_logic import question_number
-#from brain_games.brain_even_logic import game_replay
-#from brain_games.brain_even_logic import user_name
-from brain_games.cli import welcome_user
+from brain_games.brain_even_logic import player_answers
 
 
 def main():
     print("Welcome to the Brain Games!")
-    #welcome_user()
-    player_answer()
-    #print('Answer "yes" if the number is even, otherwise answer "no".')
+    player_answers()
     
-
 
 if __name__ == '__main__':
     main()
