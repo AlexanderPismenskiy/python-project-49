019e3a1587a1eb255d6cff5334dd74d8755ff8cd
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-05-30 15:21:55 +0300

    add logic for brain-calc

--- brain_games/brain_calc_logic.py
+++ brain_games/brain_calc_logic.py
@@ -1,8 +1,34 @@
 from random import choice
 from random import randint
+import prompt
+from brain_games.check_answer import calculate_correct_answer
 
-question_number_1 = randint(1, 100)
-question_number_2 = randint(1, 100)
-operators = ['+', '-', '*' ]
-question_operator = choice(operators)
+#question_number_1 = randint(1, 100)
+#question_number_2 = randint(1, 100)
+#operators = ['+', '-', '*' ]
+#question_operator = choice(operators)
 
+
+
+def player_answers():
+    number_of_attempts = 3
+    player_name = prompt.string('May I have your name? ')
+    print(f'Hello, {player_name}')
+    print('What is the result of the expression?')
+    winner = True
+    for _ in range(number_of_attempts):
+        question_number_1 = randint(1, 10)
+        question_number_2 = randint(1, 10)
+        operators = ['+', '-', '*' ]
+        question_operator = choice(operators)
+        print(f'''Question: {question_number_1} {question_operator} {question_number_2}''')
+        player_answer = prompt.integer('Your answer: ')
+        correct_answer = calculate_correct_answer(question_number_1,question_number_2, question_operator)
+        if player_answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"""'{player_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'. 
+Let's try again, {player_name}!""")
+            break
+    if winner:
+        print(f'Congratulations, {player_name}!')
