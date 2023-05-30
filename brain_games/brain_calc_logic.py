42f42e098ef6dd3d778790b7b7ac1da746dfccbf
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-05-30 14:45:25 +0300

    add brain_calc.py and brain_calc_logic.py

---
+++ brain_games/brain_calc_logic.py
@@ -0,0 +1,8 @@
+from random import choice
+from random import randint
+
+question_number_1 = randint(1, 100)
+question_number_2 = randint(1, 100)
+operators = ['+', '-', '*' ]
+question_operator = choice(operators)
+
