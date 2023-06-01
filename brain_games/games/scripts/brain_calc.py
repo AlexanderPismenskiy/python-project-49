c48bad065dd213f7fa4258e28a79172944a12f91
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-06-01 10:07:44 +0300

    create games directory

---
+++ brain_games/games/scripts/brain_calc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+from brain_games.brain_calc_logic import calc_answers
+from brain_games.games_rules import welcome_user
+
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    calc_answers()
+
+
+if __name__ == '__main__':
+    main()
