85fdcdfaaf081fcdb080ab1846c5c3dafb69ce05
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-06-01 10:17:49 +0300

    add brain_games/games/games_logic directory

---
+++ brain_games/games/games_logic/check_answer.py
@@ -0,0 +1,14 @@
+def check_answer(answer):
+    if answer is True:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def calculate_correct_answer(number_1, number_2, operator):
+    if operator == '+':
+        return  number_1 + number_2
+    if operator == '-':
+        return  number_1 - number_2
+    if operator == '*':
+        return number_1 * number_2
