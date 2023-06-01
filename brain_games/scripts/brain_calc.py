2ee1c506babd71ff43f1d541f0d3edecc65f3111
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-06-01 10:40:47 +0300

    move scripts to  brain_games/scripts

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.games_logic.brain_calc_logic import calc_answers
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    calc_answers()
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.games_logic.brain_even_logic import player_answers
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    player_answers()
+
+
+if __name__ == '__main__':
+    main()
