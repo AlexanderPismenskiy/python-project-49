15a3a0dc12e1f2b8971dd4987f0f49333ae9f332
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-06-04 13:52:42 +0300

    change brain_games/games/check_answer.py

--- brain_games/games/check_answer.py
+++ brain_games/games/check_answer.py
@@ -3,12 +3,3 @@ def check_answer(answer):
         return 'yes'
     else:
         return 'no'
-
-
-def calc_answer(number_1, number_2, operator):
-    if operator == '+':
-        return number_1 + number_2
-    if operator == '-':
-        return number_1 - number_2
-    if operator == '*':
-        return number_1 * number_2
