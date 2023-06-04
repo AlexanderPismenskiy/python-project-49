f2999adddda1106d3d91302e26614650d3d794c0
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-06-04 14:38:39 +0300

    change logic

---
+++ brain_games/games/brain_progression_logic.py
@@ -0,0 +1,25 @@
+from random import randint
+from random import choice
+
+
+GAME_RULE = 'What number is missing in the progression?'
+
+
+def make_progression():
+    first_number, difference = randint(1, 30), randint(1, 10)
+    progression_length = randint(5, 10)
+    progression = []
+    for i in range(progression_length):
+        first_number += difference
+        progression.append(first_number)
+    return progression
+
+
+def get_game():
+    progression = make_progression()
+    hiden_number = choice(progression)
+    hiden_index = progression.index(hiden_number)
+    correct_answer = str(hiden_number)
+    progression[hiden_index] = ".."
+    task = ' '.join(map(str, progression))
+    return correct_answer, task
