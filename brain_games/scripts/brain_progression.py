a9a26ff61e019a89d740141ca334f09345bd47f4
Alexander Pismenskiy <alex.pismenskiy@gmail.com>
2023-06-04 14:37:34 +0300

    change logic

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from brain_games.games_engine import engine
-from brain_games.games import brain_progression
+from brain_games.games import brain_progression_logic
 
 
 def main():
-    engine(brain_progression)
+    engine(brain_progression_logic)
 
 
 if __name__ == '__main__':
